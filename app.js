71a58864d526ed3a8a6cf6218edb84b090b0a1ce
TarchyV <thomasvallee99@gmail.com>
2021-06-10 18:35:00 -0400

    simple perlin noise generation

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 //IMPLEMENT THE PAGES HERE
